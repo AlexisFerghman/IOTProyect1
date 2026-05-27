9ad6a989f6a84c2393603bdae6d4f4f26876f0d4
AlexisFerghman <aferghman21@alumnos.utalca.cl>
2026-05-27 13:00:30 -0400

    fix

--- Camara/Camara.ino
+++ Camara/Camara.ino
@@ -50,9 +50,9 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_UXGA;
-  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
-  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  // For detection use RGB565 and a small square frame
+  config.frame_size = FRAMESIZE_QVGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for face/person detection
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
   config.jpeg_quality = 12;
